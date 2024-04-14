d373fa9a10493ae21e44209c4e7ab1a1f087cfd9
Miłosz B. <milosz.bedrylo@gmail.com>
2024-04-14 22:46:28 +0200

    parallel TODO: add required functions to menu

--- benchmarks/isBenchmark.go
+++ benchmarks/isBenchmark.go
@@ -1,11 +1,31 @@
 package benchmarks
 
+import "sync"
+
 func runIsBenchmark() {
+	var wg = sync.WaitGroup{}
 	for i := 0; i < 6; i++ {
-		SingleConfigBenchmark(i, 0, 0, 100, "i", "random")
-		SingleConfigBenchmark(i, 0, 0, 100, "i", "sorted")
-		SingleConfigBenchmark(i, 0, 0, 100, "i", "reverseSorted")
-		SingleConfigBenchmark(i, 0, 0, 100, "i", "thirdSorted")
-		SingleConfigBenchmark(i, 0, 0, 100, "i", "twoThirdsSorted")
+		wg.Add(5)
+		go func() {
+			defer wg.Done()
+			SingleConfigBenchmark(i, 0, 0, 100, "i", "random")
+		}()
+		go func() {
+			defer wg.Done()
+			SingleConfigBenchmark(i, 0, 0, 100, "i", "sorted")
+		}()
+		go func() {
+			defer wg.Done()
+			SingleConfigBenchmark(i, 0, 0, 100, "i", "reverseSorted")
+		}()
+		go func() {
+			defer wg.Done()
+			SingleConfigBenchmark(i, 0, 0, 100, "i", "thirdSorted")
+		}()
+		go func() {
+			defer wg.Done()
+			SingleConfigBenchmark(i, 0, 0, 100, "i", "twoThirdsSorted")
+		}()
 	}
+	wg.Wait()
 }
